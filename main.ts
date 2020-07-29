ae9aa220c3a2ae24ed9f75381392960662d029b1
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-07-29 19:02:16 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,50 +9,50 @@
  * 
  * 1,00 - P0
  */
-let P0 = false
-let P1 = false
-let P2 = false
+let P0 = 0
+let P1 = 0
+let P2 = 0
 basic.forever(function () {
+    P0 = 0
+    P1 = 0
+    P2 = 0
     if (input.pinIsPressed(TouchPin.P0)) {
-        P0 = true
+        P0 = 1
     } else {
-        P0 = false
+        P0 = 0
     }
     if (input.pinIsPressed(TouchPin.P1)) {
-        P1 = true
+        P1 = 1
     } else {
-        P1 = false
+        P1 = 0
     }
     if (input.pinIsPressed(TouchPin.P2)) {
-        P2 = true
+        P2 = 1
     } else {
-        P2 = false
+        P2 = 0
     }
-    basic.pause(100)
-    if (P0 && P1) {
+    if (P0 == 1 && P1 == 1) {
         basic.showNumber(0.5)
-    } else {
-        if (P1 && P2) {
-            basic.showNumber(0.1)
-        } else {
-            if (P2) {
-                basic.showNumber(0.05)
-            } else {
-                if (P1) {
-                    basic.showNumber(0.25)
-                } else {
-                    if (P0) {
-                        basic.showNumber(1)
-                    } else {
-                        basic.clearScreen()
-                    }
-                }
-            }
-        }
+        P0 = 0
+        P1 = 0
+    }
+    if (P1 == 1 && P2 == 1) {
+        basic.showNumber(0.1)
+        P1 = 0
+        P2 = 0
+    }
+    if (P2 == 1) {
+        basic.showNumber(0.05)
+        P2 = 0
+    }
+    if (P1 == 1) {
+        basic.showNumber(0.25)
+        P1 = 0
+    }
+    if (P0 == 1) {
+        basic.showNumber(1)
+        P0 = 0
     }
     basic.clearScreen()
-    basic.pause(100)
-    P0 = false
-    P1 = false
-    P2 = false
+    basic.pause(1000)
 })
